0c72ec9bd6356ab5525fe0de75e4c67b7abc16d3
MirniyPirojok <mirniypirojok@gmail.com>
2020-09-12 15:47:41 +0300

    Homework 1. Del NullPointerException from readData

--- src/test/java/com/borisov/cube/reader/CubeDataReaderTest.java
+++ src/test/java/com/borisov/cube/reader/CubeDataReaderTest.java
@@ -2,25 +2,31 @@ package com.borisov.cube.reader;
 
 import com.borisov.cube.exception.CustomException;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CubeDataReaderTest {
+    CubeDataReader cubeDataReader;
+
+    @BeforeClass
+    public void setUp() {
+        cubeDataReader = new CubeDataReader();
+    }
 
     @Test
     public void readDataPositiveTest() throws CustomException {
         List<String> expected = new ArrayList<>();
         expected.add("1.0 2.0 3.0 4.0");
 
-        CubeDataReader cubeDataReader = new CubeDataReader();
         List<String> actual = cubeDataReader.readData("data/testData.txt");
         Assert.assertEquals(expected, actual);
     }
 
-    @Test(expectedExceptions = CustomException.class)
+    @Test(expectedExceptions = {CustomException.class, NullPointerException.class})
     public void readDataNegativeTest() throws CustomException {
-        CubeDataReader cubeDataReader = new CubeDataReader();
         cubeDataReader.readData("data/111");
     }
 }
